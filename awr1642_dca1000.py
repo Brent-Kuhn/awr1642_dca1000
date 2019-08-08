1bf3c8f746348e809c2db48299b65170e7b9730d
Brent-Kuhn <bkuhn4669@flpoly.org>
2019-08-08 14:52:36 -0400

    Updated the script after testing with the device.  It now returns an array of numpy arrays that contain returned data and is sorted

--- awr1642_dca1000.py
+++ awr1642_dca1000.py
@@ -19,56 +19,47 @@ def readADC(nC):
     UDP_IP = "192.168.33.30"
     UDP_PORT = 4098
 
-    # Set socket number and the previous socket number to 0 
-    # to initialize them as ints
-    prevNum = 0
-    sNum = 0
-
     # Setup UDP socket protocol
     log.debug("Initializing socket bind.")
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
     log.debug("Socket bind complete.")
 
-    for x in range(nC):
-        try:
+    try:
+        for x in range(nC):
             # Gather data from the DCA1000
             data, addr = sock.recvfrom(1500)
 
             # Check to see if data is in order
             # Get the socket number
-            sNum = int.from_bytes(data[0:4], byteorder = "little")
-            # if (sNum != 0 & prevNum != 0):
-            #     if (sNum != (prevNum + 1)):
-            #         log.error("Socket out of order")
-                    # Add zero filling for missing sockets
+            sNum = int.from_bytes(data[0:4], byteorder="little")
+            # Add zero filling for missing sockets
 
             # Find length of data
-            sLen = int.from_bytes(data[4:10], byteorder = "little")
+            sLen = len(data)
+
+            sData = np.zeros((sLen-10)//2)
 
             # Extract the data from the socket
-            sData = int.from_bytes(data[10:sLen+1], byteorder="litle", signed=True)
+            for m,k in zip(range(10, sLen, 2), range(0,sLen-10)):
+                binByte = int.from_bytes(data[m:m+2], byteorder="little", signed=True)
+                sData[k] = binByte
 
             # Append the new data to the tempData array
             tempData.append([sNum, sData])
 
-            # Update the previus socket number to be the current socket number
-            prevNum = sNum
+    except:
+        log.debug("Socket connection seems to be lost or not found.")
 
-        except:
-            log.debug("Socket connection seems to be lost or not found.")
-
-        finally:
-            socket.close()
-            log.shutdown()
+    finally:
+        sock.close()
+        log.shutdown()
     
     # Sort the data according to the socket number
-    tempData = sorted(adcData, key=lambda adcData: adcData[0])
+    tempData = sorted(tempData, key=lambda adcData: adcData[0])
     # Strip socket numbers so that only data is sent
-    for i in range(len(adcData)):
-        adcData.append(adcData[i][1])
+    for i in range(len(tempData)):
+        adcData.append(tempData[i][1])
 
     # Return only nC ammount of data as an array
     return adcData
-
-data = readADC(3)
